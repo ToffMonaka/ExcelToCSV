dc1e1ca43741e597d05e5b0c862b8eaf88fcaa01
Toff <toff-monaka@live.jp>
2026-05-20 16:35:55 +0900

    初期コミット

---
+++ make.bat
@@ -0,0 +1,16 @@
+@echo コンパイル: 開始
+
+set CLASSPATH=%CLASSPATH%;lib\poi-5.5.1.jar
+
+rd /s /q classes
+@md classes
+
+javac -encoding UTF-8 -d classes src\com\toff_monaka\excel_to_csv\*.java
+
+jar -cvmf MANIFEST.MF bin\ExcelToCSV.jar -C classes .
+
+rd /s /q classes
+
+@echo コンパイル: 終了
+
+pause
---
+++ sample/excel_to_csv.bat
@@ -0,0 +1,7 @@
+@echo Java ExcelToCSV: äJén
+
+java -Xms256M -Xmx256M -jar ..\bin\ExcelToCSV.jar
+
+@echo Java ExcelToCSV: èIóπ
+
+pause
