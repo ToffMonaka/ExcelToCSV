7f17639797381feced3dffd7331a5079157d9d04
Toff <toff-monaka@live.jp>
2026-05-21 17:24:22 +0900

    修正

--- src/com/toff_monaka/excel_to_csv/Main.java
+++ src/com/toff_monaka/excel_to_csv/Main.java
@@ -13,16 +13,50 @@ import java.io.*;
 /**
  * @brief Mainクラス
  */
-
-public class Main {
+public class Main
+{
 	/**
 	 * @brief main関数
 	 * @param app_param_ary(application_parameter_array)
 	 */
 	public static void main(String[] app_param_ary)
 	{
-		System.out.println("Excel To CSV");
+		if (app_param_ary.length != 3) {
+			System.out.println("Error: アプリケーションパラメータ数が異常です。: excel_path csv_path charset");
+			
+			System.exit(0);
+        }
+        
+        var excel_path = app_param_ary[0];
+        
+        if (excel_path.length() <= 0) {
+			System.out.println("Error: excel_pathが空です。");
+			
+			System.exit(0);
+        }
+        
+		System.out.println("excel_path=" + excel_path);
 
-        return;
+        var csv_path = app_param_ary[1];
+
+        if (csv_path.length() <= 0) {
+			System.out.println("Error: csv_pathが空です。");
+			
+			System.exit(0);
+        }
+
+		System.out.println("csv_path=" + csv_path);
+
+        var charset = app_param_ary[2];
+
+        if (charset.length() <= 0) {
+			System.out.println("Error: charsetが空です。");
+			
+			System.exit(0);
+        }
+
+		System.out.println("charset=" + charset);
+
+		return;
 	}
 }
