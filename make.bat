ca1eb2301e930c595e364cf3a4e75789d29f69a4
Toff <toff-monaka@live.jp>
2026-05-25 19:51:50 +0900

    修正

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo コンパイル: 開始
 
-set CLASSPATH=%CLASSPATH%;lib\commons-collections4-4.5.0.jar;lib\commons-io-2.22.0.jar;lib\commons-lang3-3.20.0.jar;lib\commons-math3-3.6.1.jar;lib\commons-codec-1.22.0.jar;lib\commons-compress-1.28.0.jar;lib\log4j-api-2.26.0.jar;lib\log4j-core-2.26.0.jar;lib\SparseBitSet-1.3.jar;lib\xmlbeans-5.3.0.jar;lib\poi-5.5.1.jar;lib\poi-ooxml-5.5.1.jar;lib\poi-ooxml-lite-5.5.1.jar
+set CLASSPATH=%CLASSPATH%;lib\commons-codec-1.20.0.jar;lib\commons-io-2.21.0.jar;lib\commons-compress-1.28.0.jar;lib\commons-lang3-3.18.0.jar;lib\commons-collections4-4.5.0.jar;lib\commons-math3-3.6.1.jar;lib\poi-5.5.1.jar;lib\poi-ooxml-5.5.1.jar;lib\poi-ooxml-lite-5.5.1.jar;lib\SparseBitSet-1.3.jar;lib\curvesapi-1.08.jar;lib\log4j-api-2.24.3.jar;lib\log4j-core-2.24.3.jar;lib\xmlbeans-5.3.0.jar
 
 rd /s /q classes
 @md classes
--- src/com/toff_monaka/excel_to_csv/Main.java
+++ src/com/toff_monaka/excel_to_csv/Main.java
@@ -8,7 +8,15 @@ package com.toff_monaka.excel_to_csv;
 
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 
@@ -33,40 +41,69 @@ public class Main
 		if (app_param_ary.length != 4) {
 			System.out.println("Error: アプリケーションパラメーター数が異常です。: excel_dir_path csv_dir_path charset newline");
 			
-			System.exit(0);
-        }
-        
-        var excel_dir_path = app_param_ary[0];
-        
-        if (excel_dir_path.isEmpty()) {
+			return;
+		}
+		
+		var excel_dir_path = app_param_ary[0];
+		
+		if (excel_dir_path.isEmpty()) {
 			System.out.println("Error: excel_dir_pathが空です。");
-			
-			System.exit(0);
-        }
-        
-        var csv_dir_path = app_param_ary[1];
 
-        if (csv_dir_path.isEmpty()) {
+			return;
+		} else {
+			var excel_dir = new File(excel_dir_path);
+
+			if ((!excel_dir.exists())
+			|| (!excel_dir.isDirectory())) {
+				System.out.println("Error: excel_dir_pathが存在しないディレクトリです。: " + excel_dir_path);
+
+				return;
+			}
+		}
+		
+		var csv_dir_path = app_param_ary[1];
+
+		if (csv_dir_path.isEmpty()) {
 			System.out.println("Error: csv_dir_pathが空です。");
 			
-			System.exit(0);
-        }
+			return;
+		} else {
+			var csv_dir = new File(csv_dir_path);
 
-        var charset = app_param_ary[2];
+			if ((!csv_dir.exists())
+			|| (!csv_dir.isDirectory())) {
+				System.out.println("Error: csv_dir_pathが存在しないディレクトリです。: " + csv_dir_path);
 
-        if (charset.isEmpty()) {
+				return;
+			}
+		}
+
+		var charset = app_param_ary[2];
+
+		if (charset.isEmpty()) {
 			System.out.println("Error: charsetが空です。");
 			
-			System.exit(0);
-        }
+			return;
+		}
 
-        var newline = app_param_ary[3];
+		var newline = app_param_ary[3];
+		var newline_code = "";
 
-        if (newline.isEmpty()) {
+		if (newline.isEmpty()) {
 			System.out.println("Error: newlineが空です。");
 			
-			System.exit(0);
-        }
+			return;
+		} else {
+			if (newline.equals("CRLF")) {
+				newline_code = "\r\n";
+			} else if (newline.equals("LF")) {
+				newline_code = "\n";
+			} else {
+				System.out.println("Error: newlineが存在しない改行です。: " + newline);
+
+				return;
+			}
+		}
 
 		System.out.println("excel_dir_path=" + excel_dir_path);
 		System.out.println("csv_dir_path=" + csv_dir_path);
@@ -75,12 +112,24 @@ public class Main
 		System.out.println();
 
 		var excel_file_name_ary = com.toff_monaka.lib.data.Util.getFileNameArray(excel_dir_path, new String[] {"xls", "xlsx"});
+
+		if (excel_file_name_ary == null) {
+			System.out.println("Error: Excelファイル名配列の作成に失敗しました。");
+
+			return;
+		}
+
 		var comment_prefix = "_";
+		var sheet_name = "テーブル";
+		var invalid_column_name = "invalid_flg";
+		var invalid_column_val_ary = new String[] {"1", "1.0", ""};
+		var last_column_name = invalid_column_name;
+		var date_formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("Asia/Tokyo"));
 
 		for (var excel_file_name : excel_file_name_ary) {
-			var convert_res = 0;
-
 			System.out.println("変換開始: " + excel_file_name);
+
+			var convert_res = 0;
 
 			do {
 				var tbl_name = excel_file_name.substring(0, excel_file_name.lastIndexOf('.'));
@@ -103,13 +152,25 @@ public class Main
 					}
 				}
 
-				try (var file_is = new FileInputStream(excel_dir_path + "/" + excel_file_name); var workbook = WorkbookFactory.create(file_is)) {
-					var sheet = workbook.getSheetAt(0);
+				var file_dat = new StringBuilder();
+
+				// 読み込み
+				try (var file_is = new FileInputStream(excel_dir_path + "/" + excel_file_name);
+					var workbook = WorkbookFactory.create(file_is)) {
+					var sheet = workbook.getSheet(sheet_name);
+
+					if (sheet == null) {
+						System.out.println("Error: シートが存在しません。: " + sheet_name);
+
+						convert_res = -1;
+
+						break;
+					}
 
 					var row_cnt = sheet.getLastRowNum() - sheet.getFirstRowNum() + 1;
 
-					if (row_cnt <= 0) {
-						System.out.println("Error: ロウ数が異常です。: " + row_cnt);
+					if (row_cnt <= 1) {
+						System.out.println("Error: 行数が異常です。: " + row_cnt);
 
 						convert_res = -1;
 
@@ -119,17 +180,99 @@ public class Main
 					var column_cnt = sheet.getRow(0).getLastCellNum() - sheet.getRow(0).getFirstCellNum();
 					
 					if (column_cnt <= 0) {
-						System.out.println("Error: カラム数が異常です。: " + column_cnt);
+						System.out.println("Error: 列数が異常です。: " + column_cnt);
 						
 						convert_res = -1;
 						
 						break;
 					}
 
-					System.out.println("ロウ数=" + (row_cnt - 1));
-					System.out.println("カラム数=" + column_cnt);
+					System.out.println("行数=" + row_cnt);
+					System.out.println("列数=" + column_cnt);
+
+					var my_sheet =  new String[row_cnt][column_cnt];
+
+					for (int row_i = 0; row_i < row_cnt; ++row_i) {
+						var row = sheet.getRow(row_i);
+
+						for (int cell_i = 0; cell_i < column_cnt; ++cell_i) {
+							var val = "";
+							
+							if (row != null) {
+								var cell = row.getCell(cell_i);
+								
+								if (cell != null) {
+									switch (cell.getCellType()) {
+									case STRING: {
+										val = cell.getStringCellValue();
+
+										break;
+									}
+									case NUMERIC: {
+										if (DateUtil.isCellDateFormatted(cell)) {
+											val = date_formatter.format(cell.getDateCellValue().toInstant());
+										} else {
+											val = String.valueOf(cell.getNumericCellValue());
+
+											{// 末尾の｢.0｣を削除
+												int str_index = val.lastIndexOf(".0");
+												
+												if ((str_index >= 0) && (str_index == (val.length() - 2))) {
+													val = val.substring(0, str_index);
+												}
+											}
+										}
+
+										break;
+									}
+									case BOOLEAN: {
+										val = (cell.getBooleanCellValue()) ? "1" : "0";
+
+										break;
+									}
+									case FORMULA: {
+										val = String.valueOf(cell.getNumericCellValue());
+
+										{// 末尾の｢.0｣を削除
+											int str_index = val.lastIndexOf(".0");
+											
+											if ((str_index >= 0) && (str_index == (val.length() - 2))) {
+												val = val.substring(0, str_index);
+											}
+										}
+
+										break;
+									}
+									default: {
+										break;
+									}
+									}
+								}
+							}
+							
+							my_sheet[row_i][cell_i] = val;
+						}
+					}
 				} catch (IOException e) {
 					e.printStackTrace();
+
+					System.out.println("Error: 読み込みに失敗しました。");
+					
+					convert_res = -1;
+					
+					break;
+				}
+
+				// 書き込み
+				try (var file_os = new FileOutputStream(csv_dir_path + "/" + tbl_name + ".csv");
+					var osw = new OutputStreamWriter(file_os, charset);
+					var bw = new BufferedWriter(osw)) {
+
+					bw.write(file_dat.toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+
+					System.out.println("Error: 書き込みに失敗しました。");
 					
 					convert_res = -1;
 					
@@ -137,12 +280,14 @@ public class Main
 				}
 			} while (false);
 
-			System.out.println("変換終了: " + excel_file_name);
-			System.out.println();
-
 			if (convert_res < 0) {
 				System.out.println("Error: 変換に失敗しました。");
+
+				break;
 			}
+
+			System.out.println("変換終了: " + excel_file_name);
+			System.out.println();
 
 			{// メモリリフレッシュ
 				// 待機
